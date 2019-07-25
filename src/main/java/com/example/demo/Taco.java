08b7b3fddc073020967ab537363cd935529c9573
Marcos <marcosapmf22@gmail.com>
2019-07-24 23:19:17 -0300

    first commit

---
+++ src/main/java/com/example/demo/Taco.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class Taco {
+	private String name;
+}
