d4f5aa05e11b9c7fb1716650b9ae3b8ace0da10a
Marcos <marcosapmf22@gmail.com>
2019-07-30 21:26:01 -0300

    orderController + validations

--- src/main/java/com/example/demo/DesignTacoController.java
+++ src/main/java/com/example/demo/DesignTacoController.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.Ingredient.Type;
@@ -42,6 +43,12 @@ public class DesignTacoController {
 		model.addAttribute("design", new Taco());
 		
 		return "design";
+	}
+	
+	@PostMapping
+	public String processDesign(Taco taco) {
+		log.info("Processing design: " + taco);
+		return "redirect:/orders/current";
 	}
 
 	private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
--- src/main/java/com/example/demo/Taco.java
+++ src/main/java/com/example/demo/Taco.java
@@ -1,5 +1,10 @@
 package com.example.demo;
 
+import java.util.List;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -7,5 +12,11 @@ import lombok.RequiredArgsConstructor;
 @Data
 @NoArgsConstructor
 public class Taco {
+	
+	@NotNull
+	@Size(min=5, message="Name must be at least 5 characters long")
 	private String name;
+	
+	@Size(min=1, message="You must choose at least 1 ingredient")
+	private List<String> ingredients;
 }
